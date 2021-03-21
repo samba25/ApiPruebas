c2c067882aa9b2b7cafc51fd74ff8a334bd38f26
Leonardo <samba25leo@gmail.com>
2021-03-21 10:53:58 -0300

    BaseDTO

---
+++ ApiPruebas/Dtos/Common/BaseDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPruebas.Dtos.Common
+{
+	public abstract class BaseDto<TMe, TModel>
+	{
+		public abstract TMe FromModel(TModel value);
+		public abstract TModel ToModel();
+	}
+}
